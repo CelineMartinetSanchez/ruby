3939a19cafc37c8bddf00e260f5be6b3ed1a54c9
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-06 18:07:10 +0100

    Solution to the fifth challenge - level 0

--- Wagon-0/05-What-your-name/introduce_yourself.rb
+++ Wagon-0/05-What-your-name/introduce_yourself.rb
@@ -1,1 +1,16 @@
 # your code goes here, introduce yourself !
+
+puts 'What\'s your first name?'
+first_name = gets.chomp
+
+puts 'What\'s your middle name?'
+middle_name = gets.chomp
+
+puts 'What\'s your last name?'
+last_name = gets.chomp
+
+length = (first_name.length + middle_name.length + last_name.length)
+
+puts "Hi #{first_name} #{middle_name} #{last_name}! You have #{length} characters in your name."
+
+puts "Your're a good person!" unless (last_name.empty? || middle_name.empty? || last_name.empty?)
