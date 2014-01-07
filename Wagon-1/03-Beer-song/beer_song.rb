a8585288091a1d8219f37db3f024a27770af0e38
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-07 15:14:21 +0100

    Solution to the third challenge - level 1!

--- Wagon-1/03-Beer-song/beer_song.rb
+++ Wagon-1/03-Beer-song/beer_song.rb
@@ -1,5 +1,26 @@
 def beersong(nb_at_start)
-  # your code goes here
+	n = nb_at_start.to_i
+	bottle_a = n > 1 ? "bottles" : "bottle"
+	bottle_b = n == 2 ? "bottle" : "bottles"
+	
+		until n == 1
+			puts "#{n} #{bottle_a} of beer on the wall, #{n} #{bottle_a} of beer!"
+			puts "Take one down, pass it around, #{n - 1} #{bottle_b} of beer on the wall!"
+			n -= 1
+		end
+			
+		if n==1
+			puts "#{n} #{bottle_a} beer on the wall, #{n} #{bottle_a} of beer."
+			puts "Take one down and pass it around, no more bottles of beer on the wall."
+			n -= 1
+		end
+
+		if n == 0
+			puts "No more bottles of beer on the wall, no more bottles of beer."
+			puts "Go to the store and buy some more, 99 bottles of beer on the wall."
+			
+		end
+
 end
 
 beersong( ARGV[0] )
