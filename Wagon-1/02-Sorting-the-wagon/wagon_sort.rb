e98a850b855394931e68f7c10e76b4036d9c1dd9
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-07 12:49:00 +0100

    Solution to the second challenge - level 1!

--- Wagon-1/02-Sorting-the-wagon/wagon_sort.rb
+++ Wagon-1/02-Sorting-the-wagon/wagon_sort.rb
@@ -1,5 +1,19 @@
+
+def input_student(students_array)
+	while true
+		puts "What's the name of your student? (press enter when you finish)"
+		new_student = gets.chomp
+		break if new_student.empty?
+		students_array << new_student
+	end
+end
+
 def wagon_sort( students_array )
-  # your code to sort the array and output students
+  students_array.sort {|a, b| a.downcase <=> b.downcase}
 end
 
 # your code to interact with the user..
+students_array = Array.new
+input_student(students_array)
+puts "Bravo! You added the following students:"
+wagon_sort(students_array).each {|name| puts name}
