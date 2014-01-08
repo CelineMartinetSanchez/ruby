5d58cbd8305c8b7e392b3aef3f8a7f536a94020e
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-08 10:45:18 +0100

    Partial solution to the fifth challenge - level 1

--- Wagon-1/06-Shuffle/shuffle.rb
+++ Wagon-1/06-Shuffle/shuffle.rb
@@ -1,8 +1,26 @@
-def shuffle( array )
-  # your code goes here
+
+def shuffle(array)
+	old = array.clone
+	rand_array = []
+
+	until old.length == 0
+	  random_number = rand(old.length)
+	  rand_array << old[random_number]
+	  old.delete_at(random_number)
+	end
+	rand_array
 end
- 
-sorted_array = (1..10).to_a
- 
+
+
+def shuffle2(array)
+  array.sort_by {|x| rand}
+end
+
 # Testing your shuffle algorithm
-p shuffle(sorted_array) != shuffle(sorted_array) # => true
+#p shuffle(sorted_array) != shuffle(sorted_array) # => true
+#p shuffle2(sorted_array) != shuffle2(sorted_array) # => true
+#p shuffle2(sorted_array)
+sorted_array = (1..10).to_a
+
+p shuffle(sorted_array)
+p shuffle2(sorted_array)
