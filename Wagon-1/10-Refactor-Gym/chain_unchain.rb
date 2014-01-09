0efc236d465f469a26f6ae96c2137e92c4e3d70f
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-09 15:29:29 +0100

    Solution to the tenth challenge - level 1!!

--- Wagon-1/10-Refactor-Gym/chain_unchain.rb
+++ Wagon-1/10-Refactor-Gym/chain_unchain.rb
@@ -1,20 +1,31 @@
 def shuffle_word(a_word)
   # Ruby fonction to play to "Des chiffres et des lettres.."
   
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
+  # cap_word = a_word.upcase
+  # chars_enum = cap_word.chars
+  # array = chars_enum.to_a
+  # shuffle_array = array.shuffle
   
-  return shuffle_array
-  
+  # return shuffle_array
+  a_word.upcase.chars.to_a.shuffle
+
 end
 
 
 def quote_prime_numbers(n)
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
-end
 
+	prime = (1..n).find_all do |i|
+		dividors = (2..i-1)
+		check = dividors.select {|k| i % k == 0 }
+		check.count == 0
+	end	
+
+	write_prime = prime.map{ |prime_num| "#{prime_num} is prime"}
+
+	return write_prime
+
+
+end
 
 puts shuffle_word("nabuchodonosor")
 puts quote_prime_numbers(30)
