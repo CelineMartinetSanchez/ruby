e51189e2e8499c0624e42200a136737fe37aec30
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-09 12:25:40 +0100

    Solution to the ninth challenge - level 1!

--- Wagon-1/09-Debug-Gym/bugged_concatenate.rb
+++ Wagon-1/09-Debug-Gym/bugged_concatenate.rb
@@ -1,8 +1,15 @@
-def bugged_concatenate(array)
+def bugged_concatenate(*array)
   # 2. fix the code: TypeError
-  puts array.inject("") { |output, element| output + element }.upcase
+
+  array = array[0] if array.length == 1 and array[0].is_a?(Array)
+
+	puts array.inject("") { |output, element| output + element.to_s }.upcase
+	
 end
 
 
 # 1. fix the method call: ArgumentError
-bugged_concatenate( 1, "9", 84, " ", "George Orwell" )
+print bugged_concatenate( [1, "9", 84, " ", "George Orwell"])
+
+print bugged_concatenate("Simply happy ", 4, " the weather is", " fine")
+
