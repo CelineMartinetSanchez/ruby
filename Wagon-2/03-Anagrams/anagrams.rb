205f702e0c6f76c11bc3c2fa09139503fd18f389
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-12 19:04:54 +0100

    Solution to the third challenge - level 2

--- Wagon-2/03-Anagrams/anagrams.rb
+++ Wagon-2/03-Anagrams/anagrams.rb
@@ -1,13 +1,23 @@
 def anagrams?( a_string, another_string )
-  # your code goes here
+  # Transform in downcase, then split into letters without blank spaces and then sort fram a to z.
+  # Compare both arrays.
+  n = a_string.downcase.split(%r{\s*}).sort
+  m = another_string.downcase.split(%r{\s*}).sort
+  n == m
 end
 
 def anagrams_on_steroids?( a_string, another_string )
-  # your code goes here
+	hash1 = Hash.new { |h, k| h[k] = 0 }
+	hash2 = Hash.new { |h, k| h[k] = 0 }
+	a_string.downcase.split(%r{\s*}).each {|x| hash1[x]+=1}
+	another_string.downcase.split(%r{\s*}).each {|x| hash2[x]+=1}
+
+	hash1 == hash2
+
 end
 
 # complexity = ?
-puts anagrams? "Monica Lewinsky", "Nice silky woman" # => true 
+puts anagrams?("Monica Lewinsky", "Nice silky woman") # => true 
 
 # complexity = ?
 puts anagrams_on_steroids? "Monica Lewinsky", "Nice silky woman" # => true
