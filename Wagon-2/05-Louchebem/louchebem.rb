fa0de465f4686085d1af899b28bda200b4b48f5d
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-13 16:44:27 +0100

    Partial solution to the fifth challenge - level 2

--- Wagon-2/05-Louchebem/louchebem.rb
+++ Wagon-2/05-Louchebem/louchebem.rb
@@ -1,1 +1,12 @@
-# the file is yours !
+
+def louchebem(word)
+	word.split('').each_with_index do |letter, index|
+	 	if %w(a e i o u y).include? letter
+	 		return "L" + word[index..-1] + word[0..index-1] + %w(em oc iche uche truc).shuffle[0]
+	 end
+	end
+end
+
+puts "Quel mot voulez-vous traduire ?"
+sentence = gets.chomp
+puts louchebem(sentence)
