5d40ceeec4cfa57d7c42abea74aef5fa4fa13bdd
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-14 10:41:50 +0100

    Solution to the eigth challenge

--- Wagon-2/08-About-blocks/block_gym.rb
+++ Wagon-2/08-About-blocks/block_gym.rb
@@ -1,12 +1,33 @@
 # Recoding inject iterator
+# 1 - Refaire "inject"
 
 def inject(array, initial_value)
-  # your code goes here
+	result = initial_value
+	array.each do |val|
+		result = yield(result, val) 
+	end
+	result
 end
 
-puts inject(1..100, 0) { |initial_value, element| element + initial_value } == 5050 # true
+puts inject(1..100, 0) { |accum, element| element + accum } == 5050 # true
 
-# Block timer
+puts inject(1..4, 1) { |accum, element| element * accum } # => 24 
+
+# 2 - Pour être sûr que j'ai bien compris
+def times(n)
+	array = (1..n)
+	array.each do |val|
+		yield(val)
+	end
+end
+
+times(5) { |i| puts i }
+# 1
+# 2
+# ...
+# 5
+
+# 3 - Block timer
 
 def timer_for
   # your code goes here
