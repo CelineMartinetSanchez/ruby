32ae52c7c2134843eb3efcc77524e53740710527
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-14 14:47:49 +0100

    Solution to the first challenge - level 3

--- Wagon-3/01-Mail-spotter/parsing_mail.rb
+++ Wagon-3/01-Mail-spotter/parsing_mail.rb
@@ -1,12 +1,28 @@
-def parse_mail
+def parse_mail(mail_address)
   # 1. How do you model the jokes ? Pick up the adequate line
-  $jokes = [["gmail", "you're an average but modern person"],["lewagon", "you're skilled and capable"]] 
-  jokes = "gmail,you're an average but modern person\nlewagon;you're skilled and capable" 
-  JOKES = {"gmail" => "you're an average but modern person", "lewagon" => "you're skilled and capable"}
-  
-  # 2. Don't hesitate to add jokes to this poor list..
-  
-  # 3. Now you simply have to code the rest :)
+  #$jokes = [["gmail", "you're an average but modern person"],["lewagon", "you're skilled and capable"]] 
+  #jokes = "gmail,you're an average but modern person\nlewagon;you're skilled and capable" 
+  jokes = {
+  	gmail: "you're being watched by Big Brother bro.", 
+  	lewagon: "you're skilled and capable",
+  	caramail: "you still exist? Amazing.",
+  	yopmail: "Enjoy, your lifetime is counted."
+  }
+  if mail_address.include?("@") 
+  	# Change Name
+  	name = mail_address.match(/^[^@]*/).to_s.split(".")
+  	nice_name = name.each { |name| name.capitalize!}.join(" ")
+
+  	# Provider
+  	provider = mail_address.match(/[@](\w*)/).to_s.split(/\W/)[-1]
+
+  	# Return message
+  	"Hello #{nice_name}, #{jokes[provider.to_sym]}"
+
+  else 
+  	"This is a wrong e-mail mail_address"
+  end
+ 
 end
 
-parse_mail("boris@lewagon.org") # => "Well done boris, you're skilled and capable"
+p parse_mail("boris.loula@lewagon.org")
