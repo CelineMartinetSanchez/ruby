1324fd18a2fe8f310bf5b27587e93964dc966bc4
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-01-14 14:55:28 +0100

    Solution to the first challenge - level 3

--- Wagon-3/01-Mail-spotter/parsing_mail.rb
+++ Wagon-3/01-Mail-spotter/parsing_mail.rb
@@ -1,13 +1,18 @@
+JOKES = {
+    gmail: "you're being watched by Big Brother bro.", 
+    lewagon: "you're skilled and capable",
+    caramail: "you still exist? Amazing.",
+    yopmail: "Enjoy, your lifetime is counted.",
+    aol: "you rock!",
+    yahoo: "yeah!"
+  }
+
+
 def parse_mail(mail_address)
   # 1. How do you model the jokes ? Pick up the adequate line
   #$jokes = [["gmail", "you're an average but modern person"],["lewagon", "you're skilled and capable"]] 
   #jokes = "gmail,you're an average but modern person\nlewagon;you're skilled and capable" 
-  jokes = {
-  	gmail: "you're being watched by Big Brother bro.", 
-  	lewagon: "you're skilled and capable",
-  	caramail: "you still exist? Amazing.",
-  	yopmail: "Enjoy, your lifetime is counted."
-  }
+
   if mail_address.include?("@") 
   	# Change Name
   	name = mail_address.match(/^[^@]*/).to_s.split(".")
@@ -17,7 +22,7 @@ def parse_mail(mail_address)
   	provider = mail_address.match(/[@](\w*)/).to_s.split(/\W/)[-1]
 
   	# Return message
-  	"Hello #{nice_name}, #{jokes[provider.to_sym]}"
+  	"Hello #{nice_name}, #{JOKES[provider.to_sym]}"
 
   else 
   	"This is a wrong e-mail mail_address"
@@ -25,4 +30,6 @@ def parse_mail(mail_address)
  
 end
 
-p parse_mail("boris.loula@lewagon.org")
+puts "Enter your e-mail address"
+e_mail = gets.chomp
+p parse_mail(e_mail)
